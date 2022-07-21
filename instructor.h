32a90f4e6325f1b4a5f7c741f70d1ed4d055e492
anthonyhuynh729 <61285032+anthonyhuynh729@users.noreply.github.com>
2022-07-21 09:49:37 -0400

    Add files via upload

---
+++ instructor.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "user.h"
+class instructor :
+    public user
+{
+public:
+    instructor(string first, string last, string ID);
+    void setAttributes(string fName, string lName, string ID);
+    // Three methods for instructor class
+    void addCourse(string ID, string CRNS);
+    void removeCourse(string ID, string CRNS);
+    void printSchedule(string ID);
+    void printClassList();
+    void courseSearch();
+};
+
